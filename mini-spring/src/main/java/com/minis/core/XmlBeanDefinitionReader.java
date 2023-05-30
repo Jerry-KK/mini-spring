c2abc36ad8387408266010458cd0e1e0238bbfc3
lethekk <15868308739@163.com>
2023-05-31 01:07:10 +0800

    feat:xml解析属性和构造参数

--- mini-spring/src/main/java/com/minis/core/XmlBeanDefinitionReader.java
+++ mini-spring/src/main/java/com/minis/core/XmlBeanDefinitionReader.java
@@ -3,7 +3,12 @@ package com.minis.core;
 import com.minis.beans.BeanDefinition;
 import com.minis.beans.BeanFactory;
 import com.minis.beans.SimpleBeanFactory;
+import com.minis.inject.ArgumentValues;
+import com.minis.inject.PropertyValue;
+import com.minis.inject.PropertyValues;
 import org.dom4j.Element;
+
+import java.util.List;
 
 /**
  * @author lethe
@@ -21,7 +26,29 @@ public class XmlBeanDefinitionReader {
             String beanID = element.attributeValue("id");
             String beanClassName = element.attributeValue("class");
             BeanDefinition beanDefinition = new BeanDefinition(beanID, beanClassName);
-            simpleBeanFactory.registerBeanDefinition(beanID, beanDefinition);
+            //处理属性
+            List<Element> propertyElements = element.elements("property");
+            PropertyValues PVS = new PropertyValues();
+            for (Element e : propertyElements) {
+                String pType = e.attributeValue("type");
+                String pName = e.attributeValue("name");
+                String pValue = e.attributeValue("value");
+                PVS.addPropertyValue(new PropertyValue(pName, pType, pValue));
+            }
+            beanDefinition.setPropertyValues(PVS);
+
+            //处理构造器参数
+            List<Element> constructorArgElements = element.elements("constructor-arg");
+            ArgumentValues AVS = new ArgumentValues();
+            for(Element e:constructorArgElements) {
+                String aType = e.attributeValue("type");
+                String aName = e.attributeValue("name");
+                String aValue = e.attributeValue("value");
+                AVS.addGenericArgumentValue(aValue, aType);
+            }
+            beanDefinition.setConstructorArgumentValues(AVS);
+
+            this.simpleBeanFactory.registerBeanDefinition(beanID, beanDefinition);
         }
     }
 }
